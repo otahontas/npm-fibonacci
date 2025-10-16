bf1f7214011196347f8916916455e298589b1bc7
Otto Ahoniemi <otto@ottoahoniemi.fi>
2025-10-16 22:48:42 +0300

    1.0.1

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-import packageJson from "./package.json" with { type: 'json' }; 
-export default Number(packageJson["version"].split(".")[0]);
+import nMinus1 from "n-1";
+import nMinus2 from "n-2";
+const result = nMinus1 + nMinus2;
+export default result;
