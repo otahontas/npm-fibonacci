e485c28f2850f824d69b1bf522db9916e8e56855
Otto Ahoniemi <otto@ottoahoniemi.fi>
2025-10-17 09:19:36 +0300

    165580141.0.0

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-import nMinus1 from "63245986.0.0";
-import nMinus2 from "39088169.0.0";
+import nMinus1 from "102334155.0.0";
+import nMinus2 from "63245986.0.0";
 export default nMinus1 + nMinus2;
